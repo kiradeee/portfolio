2f8350a9334b273be9b823310ae9a1c5698d2d41
emils <emils@careindeed.com>
2024-05-01 20:04:11 +0800

    Added sidebar and styling

---
+++ portfolio/src/components/Layout/index.tsx
@@ -0,0 +1,35 @@
+import { Link, NavLink } from "react-router-dom";
+import "./index.scss";
+import Logo from "../../assets/images/logo-s.png";
+import LogoSubtitle from "../../assets/images/logo_sub.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faHome, faUser } from "@fortawesome/free-solid-svg-icons";
+
+const SideBar = () => {
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to="/">
+        <img src={Logo} alt="logo" />
+        <img className="sub-logo" src={LogoSubtitle} alt="logo" />
+      </Link>
+      <nav>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink exact="true" activeclassname="active" to="/" className="about">
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          to="/"
+          className="contact"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+      </nav>
+    </div>
+  );
+};
+
+export default SideBar;
